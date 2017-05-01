e1e866d9a91eb1a8954f16b0b3523be85d1a8b0e
roberto <roberto.menivar.hz@gmail.com>
2017-05-01 13:41:20 -0600

    carpetas

--- src/java/controller/homecontroller.java
+++ src/java/controller/homecontroller.java
@@ -5,20 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
- @Controller
-public class homecontroller {
-   
-    @RequestMapping("home.htm")
-    
-    public ModelAndView home(HttpServletRequest request){  
-        
-        ModelAndView mav =new ModelAndView();
-        mav.setViewName("home/home");
-        String var11=request.getParameter("id");
-        String var2=request.getParameter("id2");
-        mav.addObject("id", var11);
-        mav.addObject("id2", var2);
-        return mav;
-    }
-    
-}
+//@Controller
+//public class homecontroller {
+//   
+//    @RequestMapping("home.htm")
+//    
+//    public ModelAndView home(HttpServletRequest request){  
+//        
+//        ModelAndView mav =new ModelAndView();
+//        mav.setViewName("home/home");
+//        String var11=request.getParameter("id");
+//        String var2=request.getParameter("id2");
+//        mav.addObject("id", var11);
+//        mav.addObject("id2", var2);
+//        return mav;
+//    }
+//    
+//}
